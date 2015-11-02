4524697fc48437962047767555e6968c083b8182
Thomas Green <thomasgreen@u.boisestate.edu>
2015-11-02 10:43:51 -0700

    adding functionality for browse page

--- app/controllers/display_story_links_controller.rb
+++ app/controllers/display_story_links_controller.rb
@@ -1,16 +1,21 @@
 class DisplayStoryLinksController < ApplicationController
   def byName
+    @stories = Story.where(status: "Published").order(:name)
   end
 
   def byAuthor
+    @stories = Story.where(status: "Published").order(:user_id)
   end
 
   def byLocation
+    @stories = Story.where(status: "Published").order(:location)
   end
 
   def byTravelCategory
+    @stories = Story.where(status: "Published").order(:travelStyle)
   end
 
   def byStoryType
+    @stories = Story.where(status: "Published").order(:storyType)
   end
 end
