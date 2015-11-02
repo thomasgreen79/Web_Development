53d17c3db5a6a0e89e33e3190c417c81dce7fac4
Thomas Green <thomasgreen@u.boisestate.edu>
2015-11-02 16:59:25 -0700

    added search query for search links

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -40,6 +40,7 @@ class StaticPagesController < ApplicationController
       travelStyle = '%' + travelStyle + '%'
       storyType = '%' + storyType + '%'
       @searchLinks = Story.find_by_sql["SELECT Name, Id, User, Location FROM Story A, (SELECT Id as UserID FROM User WHERE LastName LIKE :last AND FirstName LIKE :first) B WHERE A.User = B.UserID AND A.Location LIKE :location AND A.TravelStyle LIKE :travelStyle AND A.StoryType LIKE :storyType AND A.Name LIKE :name AND A.Status = 'Published' ORDER BY Name;", {:last => lastName, :first => firstName, :location => location, :travelStyle => travelStyle, :storyType => storyType}]
+      redirect_to { controller: 'display_story_links', action: 'bySearch', links: @searchLinks }
     else
       if working_story?
         redirect_to pages_new_path
