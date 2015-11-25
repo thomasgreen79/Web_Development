d7617a874a513ceac5aba10c998789c6d5c93a18
Thomas Green <thomasgreen@u.boisestate.edu>
2015-11-24 23:09:53 -0700

    trying again with geocoder

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,4 @@
 class Location < ActiveRecord::Base
-  attr_accessible :address, :latitude, :longitude
   geocoded_by :ip_address, :latitude => :lat, :longitude => :lon
   after_validation :geocode 
 end
